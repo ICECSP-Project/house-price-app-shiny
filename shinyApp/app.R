67911364419fb774487cc3127c97e924d605d565
Anandu R <ananduraj32@gmail.com>
2020-12-06 20:38:35 +0530

    checkpoint

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -1,11 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 library(caret)
 library(stats)
 library(shiny)
@@ -16,12 +8,12 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             sliderInput("square", "Square Area of plot", 0, 650, 1,step = 1),
-            sliderInput("constructionTime", "construction Time of building", 1950, 2020, 1,step = 1),
+            sliderInput("constructionTime", "Year constructed", 1950, 2020, 1,step = 1),
             radioButtons("subway", "Subway nearby:",
                          c("Yes" = "Has_Subway",
                            "No" = "No_Subway")
                          ),
-            radioButtons("district", "Select district:",
+            selectInput("district", "Select district:",
                          c("Chao Yang" = "ChaoYang",
                            "Chang Ping" = "ChangPing",
                            "Dong Cheng" = "DongCheng",
@@ -36,25 +28,26 @@ ui <- fluidPage(
                            "Shun Yi" = "ShunYi",
                            "Tong Zhou" = "TongZhou"
                            )),
-    
+            submitButton("submit"),
             ),
         mainPanel(
-            tableOutput("print")
+            tableOutput("print"),
         )
     ),
-    
 )
 server <- function(input, output) {
-    
+    mdl = get(load(file ="./models/final_model.rda", .GlobalEnv))
     output$print = renderPrint({
-        mdl = load(file ="./models/final_model.rda", .GlobalEnv)
         square = as.numeric(input$square)
         constructionTime = as.numeric(input$constructionTime)
         subway = as.character(input$subway)
         district = as.character(input$district)
         data = data.frame(square,constructionTime,subway,district)
-        pred = predict(get(mdl), newdata = data)
-        cat("The price is estimated to be: ",pred)
+        pred = predict(mdl, newdata = data)
+        cat("<h1>Predicted value:</h1>")
+        cat("<h3>The price is estimated to be: ",pred,"</h3>")
+        cat("<h5>Using the parameters you can find the predicted cost of house 
+            the selected region<h5>")
     })
 }
 shinyApp(ui = ui, server = server)
